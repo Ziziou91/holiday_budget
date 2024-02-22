6aae1a3c993e299f7670f73ad4aad33d6fb5eda5
Josh Gray <joshgray91@gmail.com>
2024-02-22 21:49:22 +0000

    added spacing to improve readability.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # holiday_budget
 
+[Github Link](https://github.com/Ziziou91/holiday_budget)
+
 A simple app to calculate the total cost of a holiday. The app will ask how much various aspects of your holiday, such as flights and hotels, cost and then returns the total.
 
 ## Installation
--- holiday.py
+++ holiday.py
@@ -17,7 +17,9 @@ def get_hotel_stars() -> str:
     print(bold_text("\nWhat sort of hotel would you like to stay in?"))
     terminal_menu = TerminalMenu([i.replace("_", " ") for i in stars])
     menu_entry_index = terminal_menu.show()
+
     print(stars[menu_entry_index])
+
     return stars[menu_entry_index]
 
 def get_integer() -> int:
@@ -53,18 +55,23 @@ def main() -> None:
     print_line(char="=")
     print(f"{'*'*30}{bold_text('holiday.py')}{'*'*30}")
     print_line(char="=")
+
     city_data = load_json("cities.json")
     city = get_city_flight(city_data)
     hotel_stars = get_hotel_stars()
+
     print("How many nights will you be needing a hotel for?")
     num_nights = get_integer()
     print(f"{num_nights} nights")
+    
     print("How many days will you be hiring a car for? If you don't need a car type 0")
     car_days = get_integer()
+    
     hotel_total = hotel_cost(num_nights, hotel_stars, city, city_data)
     plane_total = plane_cost(city, city_data)
     car_total = car_rental(car_days)
     total_cost = holiday_cost(hotel_total, plane_total, car_total)
+    
     print(f"The total cost of your holiday is £{total_cost}")
     print_line(char="=")
     print(f"{'*'*27}{bold_text('holiday.py END')}{'*'*28}")
